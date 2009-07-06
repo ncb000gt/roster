6ba5ecbb7cdba270c38b696b206a08228d9f874f
Nicholas Campbell <ncampbell@axiomsoftwareinc.com>
2009-07-06 08:48:56 -0400

    Slight fixes to make sure styles apply correctly and some rewrite rules.

--- Global/onstart.js
+++ Global/onstart.js
@@ -1,6 +1,7 @@
 function roster_initialize() {
     app.log('Roster Initialization');
 
+    app.log('Creating Objects Needed for Operations');
     var ub = root.get('user_bucket');
     if (!ub) {
 	ub = new Bucket();
@@ -15,9 +16,11 @@ function roster_initialize() {
 	root.add(rb);
     }
 
+    app.log('Adding rewrite rules.');
     app.addRewriteRule('/manage_roster', '/manage_roster');
-    //app.addRewriteRule('/users_json', '/users_json');
-    //app.addRewriteRule('/roles_json', '/roles_json');
+    app.addRewriteRule('/users_json', '/users_json');
+    app.addRewriteRule('/roles_json', '/roles_json');
+    app.addRewriteRule('/getSections', '/getSections');
 
     app.log('Roster Initialization Complete');
 }
